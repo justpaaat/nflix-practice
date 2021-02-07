b3a928b0d3bb632211492f68b681a0eef12acb8a
Patrick Valentin <patrick@menschlich.org>
2021-02-07 15:50:27 +0100

    integrating sign-in form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+import HeaderContainer from "../containers/header";
+import FooterContainer from "../containers/footer";
+import { Form } from "../components";
 
 export default function Signin() {
-  return <p>Hello from the signin!</p>;
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    // firebase work here
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              value={password}
+              type="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign in
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer>Test</FooterContainer>
+    </>
+  );
 }
